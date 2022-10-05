92179bd93c24258a826f92e7b29c2e65261fa1ba
kayzee <olaniyikazeem112@gmail.com>
2022-10-05 22:15:37 +0100

    changes to proxy state variables declaration

--- proxy.sol
+++ proxy.sol
@@ -5,11 +5,6 @@ pragma solidity 0.8.9;
 contract Proxy {
     // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
 
-    uint256 TotalSupply;
-    string TokenName;
-    string TokenSymbol;
-    bool public initialized;
-
     constructor(bytes memory constructData, address contractLogic) {
         // save the code address
         assembly { // solium-disable-line
